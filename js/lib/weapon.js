09877c9f74dd235746214d434b76d4cb785fb9dc
ViiDost6 <102732975+ViiDost6@users.noreply.github.com>
2024-04-20 00:02:55 +0200

    Update weapon.js
    Added sprite trakers for main character and event listeners, such as timers for reloading weapons. Needs to be tested for using it as a base for more weapons

--- js/lib/weapon.js
+++ js/lib/weapon.js
@@ -4,32 +4,51 @@
 
 const { Weapon, Bullet } = require("phaser-ce");
 
+//----------------------------------------------------------------------
+// Universal functions all weapons use on callbacks
+
+function fullBullets(weapon)
+{
+    weapon.quantity = -1;
+    // This number sets all chargers to max
+}
+
+
+
 //----------------------------------------------------------------------------------
 // Pistol weapon
 //----------------------------------------------------------------------------------
 
-var pistol = this.add.Weapon(6, 'bullet'); 
+var pistol = this.add.weapon(6, 'bullet'); 
+var pistolIs = false;
 // creation vars are (charger-size, bullet-textures, bullet-frame, weapon-group, class-of-bullets-used)
+// bool for knowing which weapon is equipped so controls work
 
-pistol.fireFrom.set(character.x, character.y);
-pistol.input.onLeftClick.add(pistol.fire, this);
-// sets fire position, and key control
+pistol.trackSprite(character, 0, 0, true);
 
-let angle = game.physics.arcade.angleToPointer( character );
-pistol.bulletAngleOffset(angle + 90);
-// gets the angle the character looks when firing, then rotates the bullet
+if (pistolIs && Phaser.Pointer.mouse.LEFT_BUTTON == 1)
+{
+    pistol.fire(null, null, null, 0, 0);
+    // Nulls are set so fire follows the traked sprite defined
+}
+// sets fire position, and key control. angles and position are managed with spriteTraker
 
 pistol.bulletKillDistance(100);
 // pixels until the bullet instance gets killed
 
-pistol.bulletSpeed(10);
+pistol.bulletSpeed(100);
 // how many pixels per second the bullet travels
 
-pistol.fireRate(6);
-// milisecond until next bullet can be fired
+pistol.fireRate(100);
+// miliseconds until next bullet can be fired
 
-//----------------------------------------------------------------------------------
-// End of pistol definition
-//----------------------------------------------------------------------------------
+pistol.onFireLimit()
+{
+    var reload = new Timer(game, true);
 
-
+    if (Phaser.Pointer.mouse.LEFT_BUTTON == 1)
+    {
+        reload.add(1000, fullBullets, pistol, pistol);
+        reload.start();
+    }
+}
