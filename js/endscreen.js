b30c7c7d80bc947f4c0fc474e1ec9e9c9b3cf0e7
Eva María García Petrakova <102732975+ViiDost6@users.noreply.github.com>
2024-05-24 20:57:43 +0200

    tintas de la muerte
    por algún motivo si no se ha recogido ninguna tinta no dibuja nada, como si fuera null

--- js/endscreen.js
+++ js/endscreen.js
@@ -15,6 +15,15 @@ function createEndScreen()
 
     let btnHome = game.add.button(GAME_STAGE_WIDTH / 2, 500, 'btnHome', startInit, this);
     btnHome.anchor.setTo(0.5, 0.5);
+
+    let blue_tint_dead = game.add.text(250, 535, totalBlueTint, { font: "25px Kalam", fill: "#0000ff" });
+    blue_tint_dead.anchor.setTo(0.5, 0.5);
+
+    let red_tint_dead = game.add.text(595, 535, totalRedTint, { font: "25px Kalam", fill: "#FF0000" });
+    red_tint_dead.anchor.setTo(0.5, 0.5);
+
+    let total_score_dead = game.add.text(650, 150, globalScore, {font: "25px Kalam", fill: "#000000" });
+    total_score_dead.anchor.setTo(0.5, 0.5);
 }
 
 function startInit()
